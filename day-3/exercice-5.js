7d4ccbf2c1909a19a7fa5b5ac3986bbbe588ab90
Maengdok <pionaxel@gmail.com>
2021-09-01 17:19:19 +0200

    010921 few corrections

--- day-3/exercice-5.js
+++ day-3/exercice-5.js
@@ -1,12 +1,15 @@
 export const my_display_comb = () => {
+  let arr = [];
+  let str = "";
+
   for (let i = 0; i < 100; i++) {
     for (let j = 0; j < 100; j++) {
-	    console.log(i, j);
+	    str = i + " " + j;
+	    arr = [str];
       if (j == 100) {
         j = 0;
       }
     }
   }
+  return arr;
 }
-
-
